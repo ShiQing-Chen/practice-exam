62b74dcc3c369d32bcf6e5879bcff7383cf01a38
ShiQing_Chen <979196931@qq.com>
2020-02-29 18:31:15 +0800

    Init

---
+++ src/main/java/com/example/practiceexam/PracticeExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practiceexam/PracticeExamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.practiceexam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracticeExamApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
