c9a15df9fdcb526805d94c03a8c40ea9547e84ea
ShiQing_Chen <979196931@qq.com>
2020-03-12 22:51:51 +0800

    添加ckeditor4文件上传接口

--- src/main/java/com/example/practiceexam/controller/UpLoadFileController.java
+++ src/main/java/com/example/practiceexam/controller/UpLoadFileController.java
@@ -40,6 +40,18 @@ public class UpLoadFileController {
     }
 
     /**
+     * ckeditor4
+     * 文件上传接口
+     */
+    @RequestMapping(value = "/ckeditorUpload/file", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public Map ckeditorUpLoad(HttpServletResponse response, @RequestParam("upload") MultipartFile uploadFile) {
+        Map<String, Object> resultVo = this.handleFileCk(uploadFile, "practice-exam/file");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        return resultVo;
+    }
+
+    /**
      * 文件上传接口
      */
     @RequestMapping(value = "/fileUpload/file", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -87,5 +99,48 @@ public class UpLoadFileController {
             logger.error("上传文件错误：", e);
         }
         return MessageVo.fail("文件上传失败！");
+    }
+
+    /**
+     * ckeditor 专用，返回类型
+     * @param uploadFile 文件
+     * @param folderName 路径
+     * @return 结果
+     */
+    @SuppressWarnings("Duplicates")
+    private Map handleFileCk(MultipartFile uploadFile, String folderName) {
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("uploaded", 0);
+        if (uploadFile == null || uploadFile.isEmpty()) {
+            map.put("error", "获取文件失败！");
+            return map;
+        }
+        String uploadFileName = uploadFile.getOriginalFilename();
+        map.put("fileName", uploadFileName);
+        try {
+            //得到上传没后缀文件名字
+            String ext = FileUtil.getFileExt(uploadFileName);
+            // 根据随机id生成文件的新名字
+            String objectName = IdGeneratorUtils.getNewId() + ext;
+            String temp = System.getProperty("user.dir");
+            File file = new File(temp + File.separator + objectName);
+            uploadFile.transferTo(file);
+
+            String fileUrl = AliOssUtil.putObject(file, objectName, folderName);
+            boolean del = file.delete();
+            if (!del) {
+                logger.warn("临时文件删除失败: {}", file.getAbsolutePath());
+            }
+            if (StringUtils.isNotBlank(fileUrl)) {
+                map.put("url", fileUrl);
+                logger.info("######上传文件：{}，地址：{}", uploadFileName, fileUrl);
+                map.put("uploaded", 2);
+                return map;
+            }
+        } catch (IOException e) {
+            logger.error("上传文件错误：", e);
+        }
+        map.put("error", "文件上传失败！");
+        return map;
     }
 }
